02f2877b0741b96f901ff4ed0a00175621bdff98
ArtemDemba <DVL.45@mail.ru>
2021-04-27 22:58:38 +0300

    lab 3

---
+++ Lab3/main.h
@@ -0,0 +1,5 @@
+#pragma once
+void FromLeft(const int size, int** arr, int value, int leftBoarder, int rightBoarder, int topBoarder, int bottomBoarder);
+void Fromtop(const int size, int** arr, int value, int leftBoarder, int rightBoarder, int topBoarder, int bottomBoarder);
+void FromRight(const int size, int** arr, int value, int leftBoarder, int rightBoarder, int topBoarder, int bottomBoarder);
+void FromBottom(const int size, int** arr, int value, int leftBoarder, int rightBoarder, int topBoarder, int bottomBoarder);
